e390852e29d9380dc646ee8ae1a3354127e1e628
Shadowking1235 <43701529+Shadowking1235@users.noreply.github.com>
2019-04-25 10:06:11 +0400

    Update snake.py
    cocatenated

--- snake.py
+++ snake.py
@@ -59,6 +59,42 @@ while 1:
 print(f'\t\t\tcongratulation {name} you win the game ☺☻ !!!')
 print('*'*100)
 
+class Dices:
+	def one(self):
+		print('  1 ')
+		print(' ---')
+		print('| . |')
+		print(' ---')
+
+	def two(self):
+		print('  2 ')
+		print(' ---')
+		print('| : |')
+		print(' ---')
+
+	def three(self):
+		print('  3 ')
+		print(' ----')
+		print('| .: |')
+		print(' ----')
+
+	def four(self):
+		print('  4 ')
+		print(' ----')
+		print('| :: |')
+		print(' ----')
+
+	def five(self):
+		print('  5 ')
+		print(' ----')
+		print('| :.:|')
+		print(' ----')
+
+	def six(self):
+		print('  6 ')
+		print(' ----')
+		print('| ::: |')
+		print(' ----')
 
 
     
